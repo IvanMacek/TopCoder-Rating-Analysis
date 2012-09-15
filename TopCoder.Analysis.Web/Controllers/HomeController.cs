7ca8056ab38ce7ae616f365f74bbb0a7c745866e
imacek <imacek@gmail.com>
2012-09-15 19:31:48 +0200

    postavljen home page listu runda

--- TopCoder.Analysis.Web/Controllers/HomeController.cs
+++ TopCoder.Analysis.Web/Controllers/HomeController.cs
@@ -6,14 +6,7 @@ namespace TopCoder.Analysis.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
-
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            return View();
+            return RedirectToAction("List", "Rounds");
         }
     }
 }
